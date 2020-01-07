617edb01625285ce5e9a3c14c562c7e76022be92
Caleb Hiebert <calebkhiebert@gmail.com>
2020-01-07 15:16:17 -0600

    add fromObject method to context

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,7 @@
 import {Flaggable} from "./flaggable";
 import {Session} from "./session";
 import {Error} from "./context-modifier";
+import {Response} from "./response";
 
 export class Context extends Flaggable {
     public id: string;
@@ -24,6 +25,31 @@ export class Context extends Flaggable {
         this.originPlatform = "";
         this.isStart = false;
         this.isTrigger = false;
+    }
+
+    /**
+     * Converts from a plain js object into a Context class
+     * @param obj an object with the same fields as a context
+     */
+    public static fromObject(obj: any): Context {
+        const c = new Context();
+
+        c.id = obj.id;
+        c.data = obj.data;
+        c.user.data = obj.user.data;
+        c.user.id = obj.user.id;
+        c.user.platformId = obj.user.platformId;
+        c.session.data = obj.session.data;
+        c.session.ver = obj.session.ver;
+        c.text = obj.text;
+        c.originPlatform = obj.originPlatform;
+        c.originalRequest = obj.originalRequest;
+        c.isStart = obj.isStart;
+        c.isTrigger = obj.isTrigger;
+        c.errors = obj.errors;
+        c.response = obj.response;
+
+        return c;
     }
 }
 
